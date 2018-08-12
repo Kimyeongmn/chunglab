aba4877e1f78b2875c94a2fe88a53418de375b76
Jaesang Yoo <jsyoo@classting.com>
2018-08-12 12:05:25 +0900

    feat: add chunglab v2

---
+++ themes/chunglab/source/metronic/assets/pages/scripts/portfolio.js
@@ -0,0 +1,67 @@
+var Portfolio = function () {
+
+
+  return {
+    //main function to initiate the module
+    init: function () {
+
+      if(location.hash){
+console.log(location.hash);
+    var projectName = location.hash;
+    var categoryName = location.hash;
+    var projectLink = $("a[href=" + projectName + "]");
+    var categoryLink = $(".filter").filter(function() { return $(this).data("filter") == categoryName.substr(1); });
+    if(categoryLink.length > 0){
+      $('.mix-grid').mixitup();
+      categoryLink.click();
+    } else if(projectLink.length > 0){
+      showProject(projectLink.parent().parent().parent());
+	} else{
+      $('.mix-grid').mixitup();
+	}
+      } else {
+    $('.mix-grid').mixitup();
+      }
+
+      doCommon();
+    
+      function doCommon(){
+        $('.mix-preview, .mix-link').click(function(){
+          showProject($(this).parent().parent().parent());
+        });
+
+        $(".mix-filter, .back").click(function(){
+
+          $(".mix_all").removeClass("col-xs-12");
+          $(".mix_all").addClass("col-sm-6");
+          $(".mix_all").addClass("col-md-4");
+
+          $(".mix-info").hide();
+          $(".mix-details").show();
+          $(".mix-grid").children().show();
+           location.hash = "";
+        });
+      }
+
+      function showProject(root){
+	  console.log("here");
+	  $(".mix-grid").children().hide();
+	  root.show();
+	  root.removeClass("col-md-4");
+	  root.removeClass("col-sm-6");
+	  root.addClass("col-xs-12");
+	  root.find(".mix-details").hide();
+	  root.find(".mix-info").show();
+      }
+
+      function hideProject(root){
+
+      }
+
+      function showCategory(root){
+        root.trigger("click");
+      }
+    }
+  };
+
+}();
